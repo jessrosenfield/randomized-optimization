6096e128c663b7b603f19c7eeb3aa33e39d74a8c
Jessica Rosenfield <rosenfield.jess@gmail.com>
2016-03-10 23:09:05 -0500

    Finish writing pretty test.

--- src/PrettyGraphTest.java
+++ src/PrettyGraphTest.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
  * Created by Jessica on 3/10/2016.
  */
 public class PrettyGraphTest {
-    public static final int ITER = 101;
+    public static final int ITER = 10000;
 
     public static void main(String[] args) {
         PrettyGraph graph = new PrettyGraph();
@@ -30,7 +30,7 @@ public class PrettyGraphTest {
 
         double start, trainingTime, end = 0;
 
-
+        System.out.println("---RHC---");
         RandomizedHillClimbing rhc = new RandomizedHillClimbing(hcp);
         FixedIterationTrainerMod fit = new FixedIterationTrainerMod(rhc, ITER);
         start = System.nanoTime();
@@ -39,15 +39,13 @@ public class PrettyGraphTest {
         trainingTime = end - start;
         trainingTime /= Math.pow(10,9);
         PrintWriter pw = new PrintWriter(System.out);
-        for (boolean[] row : graph.matrix.matrix) {
-            for (boolean b : row) {
-                pw.print(b + ",");
-            }
-            pw.print('\n');
+        for (AdjacencyMatrix.Edge edge : graph.edges) {
+           pw.println(edge.v1 + ", " + edge.v2);
         }
         pw.println("RHC: " + ef.value(rhc.getOptimal()) + " Time: " + trainingTime);
         pw.println(rhc.getOptimal());
 
+        System.out.println("---SA---");
         SimulatedAnnealing sa = new SimulatedAnnealing(1E11, .95, hcp);
         fit = new FixedIterationTrainerMod(sa, ITER);
         start = System.nanoTime();
@@ -59,6 +57,7 @@ public class PrettyGraphTest {
         pw.println("SA: " + ef.value(sa.getOptimal()) + " Time: " + trainingTime);
         pw.println(sa.getOptimal());
 
+        System.out.println("---GA---");
         StandardGeneticAlgorithm ga = new StandardGeneticAlgorithm(200, 100, 10, gap);
         fit = new FixedIterationTrainerMod(ga, ITER);
         start = System.nanoTime();
@@ -71,20 +70,18 @@ public class PrettyGraphTest {
         pw.println("GA: " + ef.value(ga.getOptimal()) + " Time: " + trainingTime);
         pw.println(ga.getOptimal());
 
-        try {
-            MIMIC mimic = new MIMIC(24, 20, pop);
-            fit = new FixedIterationTrainerMod(mimic, ITER);
-            start = System.nanoTime();
-            fit.train(start);
-            end = System.nanoTime();
-            trainingTime = end - start;
-            trainingTime /= Math.pow(10, 9);
+        System.out.println("---MIMIC---");
+        MIMIC mimic = new MIMIC(24, 20, pop);
+        fit = new FixedIterationTrainerMod(mimic, ITER);
+        start = System.nanoTime();
+        fit.train(start);
+        end = System.nanoTime();
+        trainingTime = end - start;
+        trainingTime /= Math.pow(10, 9);
 
-            pw.println("MIMIC: " + ef.value(mimic.getOptimal()) + " Time: " + trainingTime);
-            pw.println(mimic.getOptimal());
-        } catch (Error e) {
-            int i = 0;
-        }
+        pw.println("MIMIC: " + ef.value(mimic.getOptimal()) + " Time: " + trainingTime);
+        pw.println(mimic.getOptimal());
+
         pw.flush();
     }
 }
